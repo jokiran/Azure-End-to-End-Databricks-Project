bb5fb15f7a915699753becc0fcaf80ecd9c97681
kiranpmane@outlook.com <kiranpmane@outlook.com>
2026-08-05 14:27:50 +0000

    date_utils.py changed

--- src/date_utils.py
+++ src/date_utils.py
@@ -8,15 +8,13 @@ def convert_mixed_date(df, column_name):
             col(column_name).rlike("^[0-9]+$"),
             expr(f"date_add('1899-12-30', CAST({column_name} AS INT))")
         ).otherwise(
-            expr(f"""
             coalesce(
-                try_to_date({column_name}, 'yyyy-MM-dd HH:mm:ss'),
-                try_to_date({column_name}, 'yyyy-MM-dd'),
-                try_to_date({column_name}, 'M/d/yyyy'),
-                try_to_date({column_name}, 'MM/dd/yyyy'),
-                try_to_date({column_name}, 'd MMM yyyy'),
-                try_to_date({column_name}, 'd-MMM-yy')
+                to_timestamp(col(column_name), "yyyy-MM-dd HH:mm:ss").cast("date"),
+                to_timestamp(col(column_name), "yyyy-MM-dd").cast("date"),
+                to_timestamp(col(column_name), "M/d/yyyy").cast("date"),
+                to_timestamp(col(column_name), "MM/dd/yyyy").cast("date"),
+                to_timestamp(col(column_name), "d MMM yyyy").cast("date"),
+                to_timestamp(col(column_name), "d-MMM-yy").cast("date")
             )
-            """)
         )
     )
